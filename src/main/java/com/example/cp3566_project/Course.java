9e578f8dbb28a8cf02b22a5f23de5d4f19f0e9c1
Buildertoo <102919008+Buildertoo@users.noreply.github.com>
2023-11-24 12:11:10 -0330

    Student and Course classes created

---
+++ src/main/java/com/example/cp3566_project/Course.java
@@ -0,0 +1,49 @@
+package com.example.cp3566_project;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long courseId;
+    private String courseName;
+    private String courseNumber;
+    private int capacity;
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
