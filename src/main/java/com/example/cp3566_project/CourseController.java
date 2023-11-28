e8194d899be14480cf89a51ec7a5149e21abfec8
Buildertoo <102919008+Buildertoo@users.noreply.github.com>
2023-11-28 13:21:27 -0330

    Changes to routes and props

--- src/main/java/com/example/cp3566_project/CourseController.java
+++ src/main/java/com/example/cp3566_project/CourseController.java
@@ -1,6 +1,5 @@
 package com.example.cp3566_project;
 
-import org.checkerframework.checker.units.qual.C;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
